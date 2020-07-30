c8d37293bff1064df34ed095918ff9f67146f750
Vinciallym <viniciuscosta.developer@gmail.com>
2020-07-30 00:13:11 -0300

    Finished First Part

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:first/random_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'tartup Name Generator',
+      home: RandomWords(),
+    );
+  }
+}
