c375e6fdc361cd45fea3500a98759a30ecb02c12
Vinciallym <viniciuscosta.developer@gmail.com>
2020-07-30 01:22:19 -0300

    Finished Second Part

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'tartup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.white
+      ),
       home: RandomWords(),
     );
   }
